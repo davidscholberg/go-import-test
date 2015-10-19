c0b8054b94c51ca623bebbac329166bf9f68c359
David Scholberg <recombinant.vector@gmail.com>
2015-10-18 20:16:47 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/davidscholberg/go-import-test/subpkg"
+)
+
+func main() {
+    subpkg.Test()
+}
---
+++ subpkg/subpkg.go
@@ -0,0 +1,9 @@
+package subpkg
+
+import (
+    "fmt"
+)
+
+func Test() {
+    fmt.Println("yo, from github.com/davidscholberg/go-import-test/subpkg!")
+}
